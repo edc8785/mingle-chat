e8d16f922c4809196e0ba5dcfead545376b2d443
ryong <edc8785@naver.com>
2023-10-20 19:42:55 +0900

    modify preprocess

--- recommend/preprocess.py
+++ recommend/preprocess.py
@@ -8,6 +8,8 @@ from kiwipiepy import Kiwi
 
 kiwi = Kiwi()
 keyword_model = KeyBERT()
+w2v_model = gensim.models.Word2Vec.load('./recommend/model/ko.bin')
+
 
 ## remove special characters
 def text_cleaning(text):
@@ -26,13 +28,26 @@ def noun_extractor(text):
 ## extract keywords top 5
 def keyword_extractor(noun_list):
     noun_text = ' '.join(noun_list)
-    keyword_model.extract_keywords(noun_text, keyphrase_ngram_range=(1,1), top_n=5)
-    return noun_text
+    keyword_list = keyword_model.extract_keywords(noun_text, keyphrase_ngram_range=(1,1), top_n=5)
+    return keyword_list
 
 
-## convert word to vector
-def word_2_vec(word):
-    vec = 0
+## convert keywords to mean vector
+def word_2_vec(keyword_list):
+    keyword_list = [x[0] for x in keyword_list]
+    keyword_list2 = []
+
+    for i in range(0,len(keyword_list)):
+        try:
+            _ = w2v_model.wv[keyword_list[i]]
+            keyword_list2.append(keyword_list[i])
+        except:
+            pass
+
+    try:
+        vec = np.mean(w2v_model.wv[keyword_list2], axis=0)
+    except:
+        vec = np.zeros(shape=(200,))
     return vec
 
 
@@ -42,8 +57,5 @@ sample_text = "좋아하는 일로 행복하게 일하는 상위 1% 밀레니얼
 cleaned_text = text_cleaning(sample_text)
 noun_extract_text = noun_extractor(cleaned_text)
 keyword_noun_text = keyword_extractor(noun_extract_text)
+word_2_vec(keyword_noun_text)
 
-w2v_model = gensim.models.Word2Vec.load('./recommend/model/ko.bin')
-a = w2v_model.wv.most_similar("강아지")
-
-w2v_model.wv["강아지"].shape
