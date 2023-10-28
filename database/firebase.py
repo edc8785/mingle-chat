f6b5fe8a58f579f14fac73223656514c61158cb6
ryong <edc8785@naver.com>
2023-10-28 20:31:28 +0900

    firebase.py

--- database/firebase.py
+++ database/firebase.py
@@ -14,11 +14,12 @@ class UserManagement:
         )
 
         # Firebase Admin SDK 초기화
-        cred = credentials.Certificate(service_account_key_path)
-        firebase_admin.initialize_app(cred)
+        ## cred = credentials.Certificate(service_account_key_path)
+        ## firebase_admin.initialize_app(cred)
 
     # 토큰 검증 함수
     def verify_firebase_token(self, id_token):
+        """
         try:
             decoded_token = auth.verify_id_token(id_token)
             return decoded_token
@@ -26,55 +27,58 @@ class UserManagement:
             return "Token has expired"
         except auth.InvalidIdTokenError:
             return "Invalid token"
-
+        """
+        return True
+        
     # 사용자 등록
-    def register_user(self, username, password):
-        with self.connection.cursor() as cursor:
-            insert_user_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
-            cursor.execute(insert_user_query, (username, password))
+    def register_user(self, email, password, activity_name, message, description, interest_category):
+        cursor = self.connection.cursor()
+        insert_user_query = "INSERT INTO users (email, password, activity_name, message, description, interest_category) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(insert_user_query, (email, password, activity_name, message, description, interest_category))
         self.connection.commit()
 
     # 사용자 탈퇴 (추가)
-    def delete_user(self, username):
-        with self.connection.cursor() as cursor:
-            delete_user_query = "DELETE FROM users WHERE username = %s"
-            cursor.execute(delete_user_query, (username,))
+    def delete_user(self, email):
+        cursor = self.connection.cursor()
+        delete_user_query = "DELETE FROM users WHERE email = %s"
+        cursor.execute(delete_user_query, (email,))
         self.connection.commit()
 
     # 사용자 로그인
-    def login(self, username, password):
-        with self.connection.cursor() as cursor:
-            select_user_query = "SELECT * FROM users WHERE username = %s"
-            cursor.execute(select_user_query, (username,))
-            user = cursor.fetchone()
-            if user and user[2] == password:
-                return True
-            return False
-
+    def login(self, email, password):
+        cursor = self.connection.cursor()
+        select_user_query = "SELECT * FROM users WHERE email = %s"
+        cursor.execute(select_user_query, (email,))
+        user = cursor.fetchone()
+        if user and user[2] == password:
+            return True
+        return False
+    
     # 사용자 글 저장
-    def save_user_post(self, username, content):
-        with self.connection.cursor() as cursor:
-            select_user_id_query = "SELECT id FROM users WHERE username = %s"
-            cursor.execute(select_user_id_query, (username,))
-            user_id = cursor.fetchone()
-            if user_id:
-                insert_post_query = "INSERT INTO posts (user_id, content) VALUES (%s, %s)"
-                cursor.execute(insert_post_query, (user_id[0], content))
-                self.connection.commit()
+    def save_user_post(self, email, content_title, content_main):
+        cursor = self.connection.cursor()
+        select_user_id_query = "SELECT user_id FROM users WHERE email = %s"
+        cursor.execute(select_user_id_query, (email,))
+        user_id = cursor.fetchone()
+        if user_id:
+            insert_post_query = "INSERT INTO user_posts (user_id, email, content_title, content_main) VALUES (%s, %s, %s, %s)"
+            cursor.execute(insert_post_query, (user_id[0], email, content_title, content_main))
+            self.connection.commit()
 
     # 사용자 게시글 조회 (추가)
-    def get_user_posts(self, username):
-        with self.connection.cursor() as cursor:
-            select_posts_query = "SELECT content FROM posts WHERE user_id = (SELECT id FROM users WHERE username = %s)"
-            cursor.execute(select_posts_query, (username,))
-            posts = cursor.fetchall()
-            return [post[0] for post in posts]
+    def get_user_post(self, email):
+        cursor = self.connection.cursor()
+        select_post_query = "SELECT content_title, content_main FROM user_posts WHERE user_id = (SELECT user_id FROM users WHERE email = %s)"
+        cursor.execute(select_post_query, (email,))
+        posts = cursor.fetchall()
+        return [[post[0], post[1]] for post in posts]
+
 
     # 사용자 게시글 삭제 (추가)
-    def delete_user_post(self, username, post_id):
-        with self.connection.cursor() as cursor:
-            delete_post_query = "DELETE FROM posts WHERE user_id = (SELECT id FROM users WHERE username = %s) AND id = %s"
-            cursor.execute(delete_post_query, (username, post_id))
+    def delete_user_post(self, email, post_id):
+        cursor = self.connection.cursor()
+        delete_post_query = "DELETE FROM user_posts WHERE user_id = (SELECT user_id FROM users WHERE email = %s) AND post_id = %s"
+        cursor.execute(delete_post_query, (email, post_id))
         self.connection.commit()
 
 
--- main.py
+++ main.py
@@ -1,10 +1,46 @@
 from database import *
 from flask import Flask, request, jsonify
 
-
 app = Flask(__name__)
 
-@app.route("/get_posts", methods=["GET"])
+
+@app.route('/')
+def home():
+    return 'mingle-chat'
+    
+# 회원가입 API
+@app.route('/create_user', methods=['POST'])
+def create_user_api():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+    user_id = firebase.register_user(email, password)
+    return jsonify({"message": "User created successfully", "user_id": user_id})
+
+# 로그인 API
+@app.route('/login', methods=['POST'])
+def login_api():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+    user_id = firebase.login_user(email, password)
+    if isinstance(user_id, str):
+        return jsonify({"error": user_id}), 401
+    return jsonify({"message": "Login successful", "user_id": user_id})
+
+# 회원탈퇴 API
+@app.route('/delete_user', methods=['POST'])
+def delete_user_api():
+    data = request.get_json()
+    user_id = data['user_id']
+    result = firebase.delete_user(user_id)
+    if isinstance(result, str):
+        return jsonify({"error": result}), 400
+    return jsonify({"message": result})
+
+
+# 게시글 조회
+@app.route("/get_post", methods=["GET"])
 def get_posts():
     username = request.args.get("username")
     if username:
@@ -14,6 +50,7 @@ def get_posts():
     else:
         return jsonify({"message": "게시글 조회 실패"})
 
+# 게시글 삭제
 @app.route("/delete_post", methods=["POST"])
 def delete_post():
     username = request.json.get("username")
@@ -25,8 +62,9 @@ def delete_post():
     else:
         return jsonify({"message": "게시글 삭제 실패"})
 
-@app.route("/post", methods=["POST"])
-def post():
+# 게시글 작성
+@app.route("/create_post", methods=["POST"])
+def create_post():
     # 클라이언트에서 Firebase 토큰을 추출
     firebase_token = request.headers.get("Authorization").split(" ")[1]
 
@@ -47,7 +85,6 @@ def post():
 
 
 
-
 ## flask hosting
 if __name__ == "__main__":
     f = open('aws_token.txt', 'r') 
@@ -62,5 +99,30 @@ if __name__ == "__main__":
     service_account_key_path = "mingle-chat-fb-firebase-adminsdk-pb2jz-3db7100a19.json"
 
     user_manager = firebase.UserManagement(db_host, db_user, db_password, db_database, service_account_key_path)
-
     app.run()
+
+
+
+"""
+test code
+
+f = open('aws_token.txt', 'r') 
+lines = f.readlines()
+f.close()
+
+db_host = lines[0].replace("\n", "")
+db_port = lines[1].replace("\n", "")
+db_user = lines[2].replace("\n", "")
+db_database = lines[3].replace("\n", "")
+db_password = lines[4].replace("\n", "")
+service_account_key_path = "mingle-chat-fb-firebase-adminsdk-pb2jz-3db7100a19.json"
+
+user_manager = firebase.UserManagement(db_host, db_user, db_password, db_database, service_account_key_path)
+user_manager.register_user("test_email","test_password","test","test","test","test")
+user_manager.delete_user("test_email")
+
+user_manager.login("test_email","test_password")
+user_manager.save_user_post("test_email", "test_title", "title_main")
+user_manager.get_user_post("test_email")
+user_manager.delete_user_post("test_email",1)
+"""
